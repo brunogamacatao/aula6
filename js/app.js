89acdbe868c28af31fd1561a535a9f6de17ce3fe
Bruno Gama Catao <brunogamacatao@gmail.com>
2017-03-09 17:04:09 -0300

    Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var app = angular.module('meuApp', []);
+
+app.controller('MeuController', function($rootScope, $scope, $window) {
+    $scope.online = navigator.onLine;
+    $scope.pessoa = {};
+    $scope.pessoas = [];
+
+    $scope.adicionar = function() {
+        $scope.pessoas.push($scope.pessoa);
+        $scope.pessoa = {};
+        salvarPessoas();
+    };
+
+    $scope.remover = function(i) {
+        $scope.pessoas.splice(i, 1);
+    };
+
+    function carregarPessoas() {
+        if ($window.localStorage.pessoas) {
+            $scope.pessoas = JSON.parse($window.localStorage.pessoas);
+        }
+    }
+
+    function salvarPessoas() {
+        $window.localStorage.pessoas = JSON.stringify($scope.pessoas);
+    }
+
+    function checaConexao() {
+        $rootScope.$apply(function() {
+            $scope.online = navigator.onLine;
+        });
+    }
+
+    $window.addEventListener('online', checaConexao);
+    $window.addEventListener('offline', checaConexao);
+
+    carregarPessoas();
+});
